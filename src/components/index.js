cf2da2f65ff88c64ab0606b6dea9a95645c16373
Hancel <hancel@gmail.com>
2021-08-11 14:30:05 -0500

    Adjust path for DogImage index

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ import CheckBoxField from './CheckBox';
 import TextInputField from './TextInput';
 import TextLink from './TextLink';
 import CustomButton from './CustomButton';
-import DogImage from './DogImage';
+import DogImage from './DogImage/index';
 
 export {CheckBoxField, TextInputField, CustomButton, TextLink, DogImage};

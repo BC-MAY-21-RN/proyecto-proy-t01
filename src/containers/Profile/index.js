a54454e744a223330540c6f84bd522e9257be136
cortezroberto <cortezrobertov@gmail.com>
2021-08-23 11:56:47 -0500

    Codacy issue fixed

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Formik} from 'formik';
 import {span} from '../../i18n/es';
 import {signUpValidationSchema} from '../../constants/schemas/signUpValidationSchema';

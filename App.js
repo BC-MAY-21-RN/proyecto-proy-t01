5617c61fa9861e0f9b0287b8f953c8597b89b19f
Hancel <hancel@gmail.com>
2021-09-18 11:47:23 -0500

    Onboarding cards styled inside screen component

--- App.js
+++ App.js
@@ -4,8 +4,6 @@ import React from 'react';
 import {useGoogleConfiguration} from './src/components/helpers/firebaseSignUp';
 import {ScreenNavigator} from './src/navigators';
 
-import createStorage from 'typed-async-storage';
-
 const App = () => {
   useGoogleConfiguration();
   return (
--- src/containers/Onboarding/index.js
+++ src/containers/Onboarding/index.js
@@ -1,49 +1,92 @@
 import React from 'react';
 import Onboarding from 'react-native-onboarding-swiper';
-import {Button, Image} from 'react-native';
+import {Button, Image, Text, TouchableOpacity, View} from 'react-native';
 import {MainContainer} from '../SignUp/styledComponents';
 
-const Skip = ({...props}) => <Button title="Skip" color="#000000" {...props} />;
-const Next = ({...props}) => <Button title="Next" color="#000000" {...props} />;
+const Dots = ({selected}) => {
+  let backgroundColor;
+
+  backgroundColor = selected ? 'rgba(220, 67, 80, 1)' : 'rgba(255,255,255,1.5)';
+  return (
+    <View
+      style={{
+        width: 5,
+        height: 5,
+        marginHorizontal: 3,
+        backgroundColor,
+      }}
+    />
+  );
+};
+const Skip = ({...props}) => (
+  <TouchableOpacity
+    title="Skip"
+    color="#000"
+    {...props}
+    style={{marginHorizontal: 20, borderRadius: 20}}>
+    <Text style={{fontSize: 20, color: '#fff'}}>Saltar</Text>
+  </TouchableOpacity>
+);
+const Next = ({...props}) => (
+  <TouchableOpacity
+    title="Next"
+    color="#f5737D"
+    {...props}
+    style={{marginHorizontal: 20}}>
+    <Text style={{fontSize: 20, color: '#fff'}}>Siguiente</Text>
+  </TouchableOpacity>
+);
+const DoneButtonComponent = ({...props}) => (
+  <TouchableOpacity
+    title="Next"
+    color="#f5737D"
+    {...props}
+    style={{marginHorizontal: 20}}>
+    <Text style={{fontSize: 20, color: '#fff'}}>Iniciar</Text>
+  </TouchableOpacity>
+);
 
 const OnboardingScreen = ({navigation}) => {
   return (
     <MainContainer>
       <Onboarding
-        // SkipButtonComponent={Skip}
-        // NextButtonComponent={Next}
+        SkipButtonComponent={Skip}
+        NextButtonComponent={Next}
+        bottomBarColor={'#f5737D'}
+        DotComponent={Dots}
+        DoneButtonComponent={DoneButtonComponent}
         onSkip={() => navigation.replace('SignUp')}
-        onDone={() => navigation.replace('SignUp')}
+        onDone={() => navigation.navigate('SignUp')}
         pages={[
           {
             backgroundColor: '#fff',
             image: (
               <Image
-                source={require('../../constants/img/Welcome-Board.png')}
+                source={require('../../constants/img/Onboarding-Card-1.png')}
               />
             ),
-            title: 'Onboarding 1',
-            subtitle: 'Done with React Native Onboarding Swiper',
+            // title: 'Onboarding 1',
+            // subtitle: 'Done with React Native Onboarding Swiper',
           },
           {
             backgroundColor: '#fff',
             image: (
               <Image
-                source={require('../../constants/img/Adopt-a-friend.png')}
+                source={require('../../constants/img/Onboarding-Card-2.png')}
               />
             ),
-            title: 'Onboarding 2',
-            subtitle: 'Done with React Native Onboarding Swiper',
+            // title: 'Onboarding 2',
+            // subtitle: 'Done with React Native Onboarding Swiper',
           },
           {
             backgroundColor: '#fff',
             image: (
               <Image
-                source={require('../../constants/img/Adopt-a-friend.png')}
+                source={require('../../constants/img/Onboarding-Card-3.png')}
               />
             ),
-            title: 'Onboarding 2',
-            subtitle: 'Done with React Native Onboarding Swiper',
+            // title: '',
+            // subtitle: '',
           },
         ]}
       />

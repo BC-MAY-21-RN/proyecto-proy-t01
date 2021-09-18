d656292e3517d83866065b171fe4e71d4338e797
Hancel <hancel@gmail.com>
2021-09-18 12:48:44 -0500

    Onboarding initialize only on First Launch

--- src/navigators/Screens/index.js
+++ src/navigators/Screens/index.js
@@ -1,9 +1,10 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {LogIn, SignUp, DogsDetails, Onboarding} from '../../containers';
 import TabNavigator from '../Tab';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createStackNavigator();
 
@@ -22,13 +23,33 @@ const screens = listScreens.map(({name, component}) => (
 ));
 
 const ScreenNavigator = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{header: () => null}}>
-        {screens}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const [isFirstLaunch, setIsFirstLaunch] = useState(false);
+
+  // If state is FirstLaunch will open onboarding screen, otherwise sends user to Login
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  if (isFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch === true) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{header: () => null}}>
+          {screens}
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return <LogIn />;
+  }
 };
 
 export default ScreenNavigator;
